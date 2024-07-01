367918b1355c491bb2ffa65770fd4d864cb6ecd9
pratikjadhav0908 <159643367+pratikjadhav0908@users.noreply.github.com>
2024-07-02 00:29:57 +0530

    Create Swap2.cpp
    Swaping of two numbers without using third variable

---
+++ Swap2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter value of a & b :";
+    cin>>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+
+    cout<<"a="<<a;
+    cout<<"\nb="<<b;
+    
+}
