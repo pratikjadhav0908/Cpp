9d22d80d08480696497993f9bb1674dfb6d60d8b
pratikjadhav0908 <159643367+pratikjadhav0908@users.noreply.github.com>
2024-07-02 00:22:22 +0530

    Create HCFofnumber.cpp
    Program to calculate HCF of two number

---
+++ HCFofnumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,i;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    if(a<=0&&b<=0)
+    {
+        cout<<"enter natural number.";
+    }
+    for(i=a;i>=1;i--)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            cout<<i<<" is gfc of "<<a<<" & "<<b;
+            break;
+        }
+    }
+}
