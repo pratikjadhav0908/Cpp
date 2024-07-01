42cfea023559bf4a839c0fddc296a3dbec477d45
pratikjadhav0908 <159643367+pratikjadhav0908@users.noreply.github.com>
2024-07-02 00:23:25 +0530

    Create Table.cpp
    Program to return table of number

---
+++ Table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,i;
+    cout<<"Enter a number:";
+    cin>>a;
+    for(i=1;i<=10;i++)
+    {
+        cout<<a<<"x"<<i<<"="<<a*i<<"\n";
+    }
+}
