f32e06bcd2ddfadf7d1e409df8c52348372f1b6e
pratikjadhav0908 <159643367+pratikjadhav0908@users.noreply.github.com>
2024-07-14 09:24:24 +0530

    Create Trianglestar.cpp

---
+++ Trianglestar.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+ int a,i,j,n;
+
+ cout<<"Enter a number:";
+
+ cin>>n;
+
+ for(i=1;i<=n;i++)
+
+ {
+
+ for(j=1;j<=i;j++)
+
+ {
+
+ cout<<"* ";
+
+ }
+
+ cout<<"\n";
+
+ }
+}
